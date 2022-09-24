d9941b515c49fe7b071bc36a754063761c342354
Yabing <sherry.liuyabing@gmail.com>
2022-09-24 21:04:38 +0800

    Add game rules modal box

--- script.js
+++ script.js
@@ -249,6 +249,7 @@ function disableKeyboard() {
 }
 
 function activateKeyboard() {
+  // Enable on-screen keyboard actions: key in letters, delete, return
   const keys = document.querySelectorAll("#key");
   for (let i = 0; i < keys.length; i += 1) {
     keys[i].onclick = (event) => {
@@ -264,6 +265,7 @@ function activateKeyboard() {
       }
     };
 
+    // Computer keyboard actions with input validation
     document.onkeyup = (event) => {
       const keyPressed = event.key;
       if (keyPressed == "Backspace") {
